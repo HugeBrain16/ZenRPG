f2785a1c930c19b5d8a7b0a86ec1e47604e8940d
HugeBrain16 <joshtuck373@gmail.com>
2021-05-16 02:01:25 -0700

    added cooldowns, added help

--- src/lib/statics.py
+++ src/lib/statics.py
@@ -1,4 +1,5 @@
 hunt = ["boar", "bird"]
+cooldowns = ["hunt", "chop"]
 items = {
     "boar": {"price": 25},
     "bird": {"price": 10},
@@ -8,4 +9,16 @@ items = {
     "stone": {"price": 10},
     "leather": {"price": 18},
     "fish": {"price": 20},
+}
+help = {
+    "help": "Show this menu",
+    "inventory": "view your inventory",
+    "chop": "chop woods from the forest",
+    "hunt": "hunt creatures from the forest",
+    "balance": "view your balance",
+    "start": "start your journey!",
+    "player": "display player/user info",
+    "ping": "pong",
+    "sell": "sell your items",
+    "about": "about this bot",
 }
