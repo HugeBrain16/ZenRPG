417005364957c1a7d2746fa4ca514c929b61ed7d
HugeBrain16 <joshtuck373@gmail.com>
2021-05-16 01:59:41 -0700

    update

--- src/commands/about.py
+++ src/commands/about.py
@@ -7,6 +7,7 @@ async def _about():
     metadata.read_file("metadata.ini")
 
     embed = discord.Embed(title="About", color=0xFF00FF)
-    embed.description = f"PROGRAM:\n```Name: {metadata['program']['name']}\nVersion: v{metadata['program']['version']}```"
+    embed.description = f"{metadata['program']['description']}\nSource: {metadata['program']['source']}\n\nPROGRAM:\n```Name: {metadata['program']['name']}\nVersion: v{metadata['program']['version']}```"
+    embed.set_thumbnail(url=_about.client.user.avatar_url)
 
     await _about.message.channel.send(embed=embed)
